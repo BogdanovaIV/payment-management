7bf9089906371a2292a73d7024d75f3ce500d44e
BogdanovaIV <irinabogdanova261@gmail.com>
2025-01-29 10:48:25 +0500

    Change UserProfile to using generic views

--- django_api/user/views.py
+++ django_api/user/views.py
@@ -1,66 +1,20 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+
+from rest_framework import generics
 from .models import UserProfile
 from .serializers import UserProfileSerializer
 from django_api.permissions import IsOwnerOrAdminOrReadOnly
 
 
-class UserProfileList(APIView):
-    """ Handles retrieving a list of all UserProfile instances. """
-    def get(self, request):
-        """Returns a serialized list of all UserProfile objects."""
-        profiles = UserProfile.objects.all()
-        serializer = UserProfileSerializer(
-            profiles,
-            many=True,
-            context={'request': request}
-        )
-        return Response(serializer.data)
+class UserProfileList(generics.ListAPIView):
+    """Handles retrieving a list of all UserProfile instances."""
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
 
 
-class UserProfileDetail(APIView):
+class UserProfileDetail(generics.RetrieveUpdateAPIView):
     """
-    Handles retrieving, updating, and managing individual UserProfile
-    instances.
-
-    Attributes:
-        serializer_class: The serializer used for UserProfile objects.
+    Handles retrieving and updating an individual UserProfile instance.
     """
-
+    queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     permission_classes = [IsOwnerOrAdminOrReadOnly]
-
-    def get_object(self, pk):
-        """
-        Retrieves a UserProfile instance by primary key or raises Http404
-        if not found.
-        """
-        try:
-            user_profile = UserProfile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, user_profile)
-            return user_profile
-        except UserProfile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        """Returns the serialized data of a specific UserProfile instance."""
-        user_profile = self.get_object(pk)
-        serializer = UserProfileSerializer(
-            user_profile,
-            context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        """Updates a UserProfile instance with the provided data if valid."""
-        user_profile = self.get_object(pk)
-        serializer = UserProfileSerializer(
-            user_profile,
-            data=request.data,
-            context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
