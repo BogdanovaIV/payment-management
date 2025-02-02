898eceecc4b038fddd5c35fa46b5349c93493728
BogdanovaIV <irinabogdanova261@gmail.com>
2025-02-02 22:56:48 +0500

    Add chesked field to CurrentUserSerializer

--- django_api/django_api/serializers.py
+++ django_api/django_api/serializers.py
@@ -13,12 +13,14 @@ class CurrentUserSerializer(UserDetailsSerializer):
     """
 
     user_profile_id = serializers.ReadOnlyField(source='userprofile.id')
+    checked = serializers.ReadOnlyField(source='userprofile.checked')
     full_name = serializers.ReadOnlyField(source='get_full_name')
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
             'user_profile_id',
             'full_name',
+            'checked',
         )
 
 
