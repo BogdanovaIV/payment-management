c8dfe24dc2eab96cc3247a7c4abe01d93f8aef44
BogdanovaIV <irinabogdanova261@gmail.com>
2025-02-04 22:20:09 +0500

    Add docstring to class

--- django_api/user/serializers.py
+++ django_api/user/serializers.py
@@ -7,13 +7,19 @@ class UserProfileSerializer(serializers.ModelSerializer):
     """
     Serializer for the UserProfile model.
 
-    Adds additional read-only fields for the user's username and full name.
+    This serializer includes additional fields related to the associated User model, 
+    allowing controlled access to user details. It also provides functionality to update 
+    related user attributes within a transaction.
 
     Fields:
         id (int): The primary key of the UserProfile instance.
         username (str): The username of the associated User (read-only).
-        user_full_name (str): The full name of the associated User (read-only).
+        first_name (str): The first name of the associated User (required).
+        last_name (str): The last name of the associated User (required).
+        email (str): The email of the associated User (required).
+        full_name (str): The full name of the associated User (read-only).
         checked (bool): Indicates whether the UserProfile has been checked.
+        is_owner (bool): Indicates whether the requesting user is the owner of the profile.
     """
 
     id = serializers.ReadOnlyField()
@@ -40,6 +46,16 @@ class UserProfileSerializer(serializers.ModelSerializer):
         return request.user == obj.user
 
     def update(self, instance, validated_data):
+        """
+        Updates the UserProfile instance along with the related User model.
+
+        Args:
+            instance (UserProfile): The existing UserProfile instance.
+            validated_data (dict): The validated data for update.
+
+        Returns:
+            UserProfile: The updated UserProfile instance.
+        """
         user_data = validated_data.pop("user", {})
 
         with transaction.atomic():
