b6c79bd2ef9c578dc8951f08cb8c3d0ac2fb1ca0
BogdanovaIV <irinabogdanova261@gmail.com>
2025-02-11 16:46:36 +0500

    Remove unusable method

--- django_api/partner/filters.py
+++ django_api/partner/filters.py
@@ -39,30 +39,6 @@ class PartnerFilter(django_filters.FilterSet):
         required=False
     )
 
-    def get_queryset(self):
-        """
-        Returns a filtered queryset based on the request parameters.
-
-        This method overrides the default queryset filtering to correctly
-        interpret the 'is_own' parameter from the request. If 'is_own'
-        is provided as a string ('True' or 'False'), it converts it to a
-        boolean before applying the filter.
-
-        Returns:
-            QuerySet: The filtered queryset based on the provided filters.
-        """
-        queryset = super().get_queryset()
-
-        is_own = self.request.query_params.get('is_own', None)
-        if is_own is not None:
-            is_own_value = False
-            if is_own == 'True':
-                is_own_value = True
-
-            queryset = queryset.filter(is_own=is_own_value)
-
-        return queryset
-
     class Meta:
         model = Partner
         fields = ['trade_name', 'bin', 'partner_type', 'is_own']
