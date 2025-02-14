96dba4a0656ec3b460e413625730387fb81a03c3
BogdanovaIV <irinabogdanova261@gmail.com>
2025-02-14 23:04:19 +0500

    Add displaing messages for 400 error

--- react_frontend/src/utils/errorHandler.js
+++ react_frontend/src/utils/errorHandler.js
@@ -1,10 +1,13 @@
 export const handleRequestError = (err, showToast, t) => {
+  if (process.env.NODE_ENV === "development") {
+    console.log(err.response);
+  }
   if (!err.response) {
     showToast(t("toast.error_server_unavailable"), "danger");
   } else if (err.response.status === 500) {
     showToast(t("toast.error_server_occurred"), "danger");
   } else if (err.response.status === 400) {
-    showToast(t("toast.error_occurred"), "danger");
+    showToast(err.response?.data?.error || t("toast.error_occurred"), "danger");
   } else {
     if (process.env.NODE_ENV === "development") {
       console.log(err.response.status);
