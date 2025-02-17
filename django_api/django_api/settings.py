83c076d4091e1f67b0edd73964824540799c148e
BogdanovaIV <irinabogdanova261@gmail.com>
2025-02-17 23:16:52 +0500

    Compiling the static files

--- django_api/django_api/settings.py
+++ django_api/django_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 from decouple import config
 from django.utils.translation import gettext_lazy as _
 import dj_database_url
