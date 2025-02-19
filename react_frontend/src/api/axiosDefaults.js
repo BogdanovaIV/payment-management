140f98dec59b23faab5c3592e9d15b0de6adf346
BogdanovaIV <irinabogdanova261@gmail.com>
2025-02-19 20:21:19 +0500

    Change api for development

--- react_frontend/src/api/axiosDefaults.js
+++ react_frontend/src/api/axiosDefaults.js
@@ -1,7 +1,9 @@
 import axios from "axios";
-import { getLanguage } from '../utils/localStorage';
+import { getLanguage } from "../utils/localStorage";
 
-axios.defaults.baseURL = "/api"
+if (process.env.NODE_ENV !== "development") {
+  axios.defaults.baseURL = "/api";
+}
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 axios.defaults.headers["Accept-Language"] = getLanguage() || "en";
