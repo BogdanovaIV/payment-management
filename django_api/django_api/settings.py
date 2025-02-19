6aba2b7e81fd54100829a646e435ff6caf9d4b35
BogdanovaIV <irinabogdanova261@gmail.com>
2025-02-19 21:34:56 +0500

    Add useng DEV variable to determine database url

--- django_api/django_api/settings.py
+++ django_api/django_api/settings.py
@@ -153,9 +153,10 @@ DATABASES = {
     'default': ({
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    } if config('DEBUG', default=False, cast=bool) else dj_database_url.parse(
-        config('DATABASE_URL', default='')
-    ))
+    } if config('DEBUG', default=False, cast=bool)
+      or config('DEV', default=False, cast=bool)
+      else dj_database_url.parse(config('DATABASE_URL', default=''))
+    )
 }
 
 # Password validation
