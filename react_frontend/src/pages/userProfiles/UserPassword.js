5d53d04bf5356992809001f9b7b515c41fa69b0e
BogdanovaIV <irinabogdanova261@gmail.com>
2025-02-21 10:20:25 +0500

    Add checking the old password and the instruction

--- react_frontend/src/pages/userProfiles/UserPassword.js
+++ react_frontend/src/pages/userProfiles/UserPassword.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState, useMemo } from "react";
 
 import { useHistory, useParams } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
 
 import bgImageStyles from "../../styles/BgImage.module.css";
 import inputStyles from "../../styles/Input.module.css";
 import headerStyles from "../../styles/Header.module.css";
+import btnStyles from "../../styles/Button.module.css";
 
 import backgroundImage from "../../assets/user-profile.jpg";
 
@@ -20,6 +22,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import { useToast } from "../../contexts/ToastContext";
 import { handleRequestError } from "../../utils/errorHandler";
 import { useRedirect } from "../../hooks/useRedirect";
+import Instruction from "../../components/Instruction";
 
 const UserPasswordForm = () => {
   useRedirect("loggedOut");
@@ -27,13 +30,21 @@ const UserPasswordForm = () => {
   const currentUser = useCurrentUser();
   const { id } = useParams();
   const showToast = useToast();
+  const [showInstruction, setShowInstruction] = useState(false);
 
   const [userData, setUserData] = useState({
+    old_password: "",
     new_password1: "",
     new_password2: "",
   });
 
   const arrayFields = (t) => [
+    {
+      id: "old_password",
+      name: "old_password",
+      type: "password",
+      placeholder: t("auth.old_password"),
+    },
     {
       id: "new_password1",
       name: "new_password1",
@@ -71,12 +82,12 @@ const UserPasswordForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axiosReq.post("/dj-rest-auth/password/change/", userData);
+      await axiosReq.put("/user-profiles/password/change/", userData);
       history.goBack();
       showToast(t("toast.success_change_password"), "success");
     } catch (err) {
       if (process.env.NODE_ENV === "development") {
-        console.log(err);
+        console.log(err.response);
       }
       setErrors(err.response?.data);
       handleRequestError(err, showToast, t);
@@ -91,14 +102,68 @@ const UserPasswordForm = () => {
     }),
     []
   );
+
+  const instructionBody = (
+    <>
+      <p>{t("instructions.user_change_password.introduction")}</p>
+      <ol>
+        <li>
+          <strong>{t("instructions.user_change_password.filling_out")}</strong>
+          <ul>
+            <li>
+              <Trans
+                i18nKey="instructions.user_change_password.filling_out_desc1"
+                components={[<strong />]}
+              />
+            </li>
+            <li>
+              <Trans
+                i18nKey="instructions.user_change_password.filling_out_desc2"
+                components={[<strong />]}
+              />
+            </li>
+            <li>
+              <Trans
+                i18nKey="instructions.user_change_password.filling_out_desc3"
+                components={[<strong />]}
+              />
+            </li>
+            <li>{t("instructions.user_change_password.filling_out_desc4")}</li>
+          </ul>
+        </li>
+        <li>
+          <strong>{t("instructions.user_change_password.submitting")}</strong>
+          <ul>
+            <li>
+              <Trans
+                i18nKey="instructions.user_change_password.submitting_desc1"
+                components={[<strong />]}
+              />
+            </li>
+            <li>{t("instructions.user_change_password.submitting_desc2")}</li>
+            <li>{t("instructions.user_change_password.submitting_desc3")}</li>
+          </ul>
+        </li>
+      </ol>
+    </>
+  );
+
   return (
     <section className={bgImageStyles.BgImage} style={backgroundStyle}>
       <Container className="pt-2">
         <Row>
           <Container>
-            <h1 className={headerStyles.Header}>
-              {t("auth.edit_user_password")}
-            </h1>
+            <Row className="justify-content-center">
+              <h1 className={headerStyles.Header}>
+                {t("auth.edit_user_password")}
+              </h1>
+              <Button
+                className={`${btnStyles.ButtonIcon} ${btnStyles.OrangeIcon}`}
+                onClick={() => setShowInstruction(true)}
+              >
+                <i className="fa-solid fa-circle-question" />
+              </Button>
+            </Row>
           </Container>
         </Row>
         <Row>
@@ -134,6 +199,11 @@ const UserPasswordForm = () => {
           </Col>
         </Row>
       </Container>
+      <Instruction
+        instructionBody={instructionBody}
+        showInstruction={showInstruction}
+        setShowInstruction={setShowInstruction}
+      />
     </section>
   );
 };
