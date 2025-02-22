3e46e3c540327761acec1f11a4e0ca48d156916e
BogdanovaIV <irinabogdanova261@gmail.com>
2025-02-23 03:50:45 +0500

    Implement universal structure for instructions

--- react_frontend/src/utils/instructions.js
+++ react_frontend/src/utils/instructions.js
@@ -1,354 +1,116 @@
 import instructions from "../locales/en.json";
 
 export const getInstructionByFormName = (formName = "", t, Trans) => {
-  if (formName === "UserProfilePage") {
-    return (
-      <>
-        <p>{t("instructions.user_profile_view.introduction")}</p>
-        <ol>
-          <li>
-            <strong>
-              {t("instructions.user_profile_view.profile_information")}
-            </strong>
-            <ul>
-              <li>
-                {t("instructions.user_profile_view.profile_information_desc1")}
-              </li>
-              <li>
-                <Trans
-                  i18nKey="instructions.user_profile_view.profile_information_desc2"
-                  components={[<strong />]}
-                />
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>{t("instructions.user_profile_view.edit_profile")}</strong>
-            <ul>
-              <li>
-                <Trans
-                  i18nKey="instructions.user_profile_view.edit_profile_desc1"
-                  components={[<strong />]}
-                />
-              </li>
-              <li>{t("instructions.user_profile_view.edit_profile_desc2")}</li>
-            </ul>
-          </li>
-          <li>
-            <strong>
-              {t("instructions.user_profile_view.changing_password")}
-            </strong>
-            <ul>
-              <li>
-                <Trans
-                  i18nKey="instructions.user_profile_view.changing_password_desc1"
-                  components={[<strong />]}
-                />
-              </li>
-              <li>
-                {t("instructions.user_profile_view.changing_password_desc2")}
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>{t("instructions.loading_indicator")}</strong>
-            <ul>
-              <li>
-                <Trans
-                  i18nKey="instructions.loading_indicator_desc1"
-                  components={[<strong />]}
-                />
-              </li>
-              <li>{t("instructions.loading_indicator_desc2")}</li>
-            </ul>
-          </li>
-        </ol>
-      </>
-    );
+  let object = "Object";
+  let objects = "Objects";
+  let object_s = "Object's";
+  let nameInstruction = "object_list";
+
+  if (formName === "PartnerList") {
+    object = "Partner";
+    objects = "Partners";
+    object_s = "Partner's";
+  } else if (formName === "PaymentRequestsList") {
+    object = "Payment Request";
+    objects = "Payment Requests";
+    object_s = "Payment Request's";
+  } else if (formName === "UserProfilePage") {
+    nameInstruction = "user_profile_view";
   } else if (formName === "UserProfileEditForm") {
-    return (
-      <>
-        <p>{t("instructions.user_profile_edit.introduction")}</p>
-        <ol>
-          <li>
-            <strong>
-              {t("instructions.user_profile_edit.profile_information")}
-            </strong>
-            <ul>
-              <li>
-                <Trans
-                  i18nKey="instructions.user_profile_edit.profile_information_desc1"
-                  components={[<strong />]}
-                />
-              </li>
-              <li>
-                <Trans
-                  i18nKey="instructions.user_profile_edit.profile_information_desc2"
-                  components={[<strong />]}
-                />
-                <ul>
-                  <li>
-                    <Trans
-                      i18nKey="instructions.user_profile_edit.profile_information_desc2_field1"
-                      components={[<strong />]}
-                    />
-                  </li>
-                  <li>
-                    <Trans
-                      i18nKey="instructions.user_profile_edit.profile_information_desc2_field2"
-                      components={[<strong />]}
-                    />
-                  </li>
-                  <li>
-                    <Trans
-                      i18nKey="instructions.user_profile_edit.profile_information_desc2_field3"
-                      components={[<strong />]}
-                    />
-                  </li>
-                </ul>
-              </li>
-              <li>
-                {t("instructions.user_profile_edit.profile_information_desc3")}
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>
-              {t("instructions.user_profile_edit.saving_changes")}
-            </strong>
-            <ul>
-              <li>
-                <Trans
-                  i18nKey="instructions.user_profile_edit.saving_changes_desc1"
-                  components={[<strong />]}
-                />
-              </li>
-              <li>
-                {t("instructions.user_profile_edit.saving_changes_desc2")}
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>
-              {t("instructions.user_profile_edit.error_handling")}
-            </strong>
-            <ul>
-              <li>
-                {t("instructions.user_profile_edit.error_handling_desc1")}
-              </li>
-              <li>
-                {t("instructions.user_profile_edit.error_handling_desc2")}
-              </li>
-              <li>
-                {t("instructions.user_profile_edit.error_handling_desc3")}
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>
-              {t("instructions.user_profile_edit.canceling_changes")}
-            </strong>
-            <ul>
-              <li>
-                <Trans
-                  i18nKey="instructions.user_profile_edit.canceling_changes_desc1"
-                  components={[<strong />]}
-                />
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>{t("instructions.loading_indicator")}</strong>
-            <ul>
-              <li>
-                <Trans
-                  i18nKey="instructions.loading_indicator_desc1"
-                  components={[<strong />]}
-                />
-              </li>
-              <li>{t("instructions.loading_indicator_desc2")}</li>
-            </ul>
-          </li>
-        </ol>
-        <p>
-          <Trans
-            i18nKey="instructions.user_profile_edit.note"
-            components={[<strong />]}
-          />
-        </p>
-      </>
-    );
+    nameInstruction = "user_profile_edit";
   } else if (formName === "UserPassword") {
-    return (
-      <>
-        <p>{t("instructions.user_change_password.introduction")}</p>
-        <ol>
-          <li>
-            <strong>
-              {t("instructions.user_change_password.filling_out")}
-            </strong>
-            <ul>
-              <li>
-                <Trans
-                  i18nKey="instructions.user_change_password.filling_out_desc1"
-                  components={[<strong />]}
-                />
-              </li>
-              <li>
-                <Trans
-                  i18nKey="instructions.user_change_password.filling_out_desc2"
-                  components={[<strong />]}
-                />
-              </li>
-              <li>
-                <Trans
-                  i18nKey="instructions.user_change_password.filling_out_desc3"
-                  components={[<strong />]}
-                />
-              </li>
-              <li>
-                {t("instructions.user_change_password.filling_out_desc4")}
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>{t("instructions.user_change_password.submitting")}</strong>
-            <ul>
-              <li>
-                <Trans
-                  i18nKey="instructions.user_change_password.submitting_desc1"
-                  components={[<strong />]}
-                />
-              </li>
-              <li>{t("instructions.user_change_password.submitting_desc2")}</li>
-              <li>{t("instructions.user_change_password.submitting_desc3")}</li>
-            </ul>
-          </li>
-        </ol>
-      </>
-    );
+    nameInstruction = "user_change_password";
   } else if (formName === "SignUpForm") {
-    return (
-      <>
-        <p>{t("instructions.signup.introduction")}</p>
-        <ol>
-          <li>
-            <strong>{t("instructions.signup.filling_out")}</strong>
-            <ul>
-              <li>
-                <Trans
-                  i18nKey="instructions.signup.filling_out_desc1"
-                  components={[<strong />]}
-                />
+    nameInstruction = "signup";
+  } else {
+    return <></>;
+  }
+
+  return (
+    <>
+      <p>
+        <Trans
+          i18nKey={`instructions.${nameInstruction}.introduction`}
+          values={{
+            object,
+            objects,
+            object_s,
+          }}
+          components={[<strong />]}
+        />
+      </p>
+      <ol>
+        {Object.entries(instructions.instructions[nameInstruction]).map(
+          ([key, section]) =>
+            key !== "introduction" &&
+            key !== "note" && (
+              <li key={key}>
+                <strong>
+                  {t(`instructions.${nameInstruction}.${key}.title`)}
+                </strong>
                 <ul>
-                  <li>{t("instructions.signup.filling_out_desc1_ul1")}</li>
-                  <li>{t("instructions.signup.filling_out_desc1_ul2")}</li>
+                  {Object.entries(section).map(([subKey, subDesc]) =>
+                    subKey.endsWith("_dict") ? (
+                      <li key={subKey}>
+                        <strong>
+                          {t(
+                            `instructions.${nameInstruction}.${key}.${subKey}.title`
+                          )}
+                        </strong>
+                        <ul>
+                          {Object.entries(subDesc).map(
+                            ([subKeyFields, subDescFields]) =>
+                              subKeyFields !== "title" && (
+                                <li key={subKeyFields}>
+                                  <Trans
+                                    i18nKey={`instructions.${nameInstruction}.${key}.${subKey}.${subKeyFields}`}
+                                    values={{
+                                      object,
+                                      objects,
+                                      object_s,
+                                    }}
+                                    components={[<strong />]}
+                                  />
+                                </li>
+                              )
+                          )}
+                        </ul>
+                      </li>
+                    ) : subKey !== "title" ? (
+                      <li key={subKey}>
+                        <Trans
+                          i18nKey={`instructions.${nameInstruction}.${key}.${subKey}`}
+                          values={{
+                            object,
+                            objects,
+                            object_s,
+                          }}
+                          components={[<strong />]}
+                        />
+                      </li>
+                    ) : null
+                  )}
                 </ul>
               </li>
-              <li>
-                <Trans
-                  i18nKey="instructions.signup.filling_out_desc2"
-                  components={[<strong />]}
-                />
-                <ul>
-                  <li>{t("instructions.signup.filling_out_desc2_ul1")}</li>
-                </ul>
-              </li>
-              <li>
-                <Trans
-                  i18nKey="instructions.signup.filling_out_desc3"
-                  components={[<strong />]}
-                />
-                <ul>
-                  <li>{t("instructions.signup.filling_out_desc3_ul1")}</li>
-                </ul>
-              </li>
-              <li>
-                <Trans
-                  i18nKey="instructions.signup.filling_out_desc4"
-                  components={[<strong />]}
-                />
-                <ul>
-                  <li>{t("instructions.signup.filling_out_desc4_ul1")}</li>
-                  <li>{t("instructions.signup.filling_out_desc4_ul2")}</li>
-                </ul>
-              </li>
-              <li>
-                <Trans
-                  i18nKey="instructions.signup.filling_out_desc5"
-                  components={[<strong />]}
-                />
-                <ul>
-                  <li>{t("instructions.signup.filling_out_desc5_ul1")}</li>
-                  <li>{t("instructions.signup.filling_out_desc5_ul2")}</li>
-                </ul>
-              </li>
-              <li>
-                <Trans
-                  i18nKey="instructions.signup.filling_out_desc6"
-                  components={[<strong />]}
-                />
-                <ul>
-                  <li>{t("instructions.signup.filling_out_desc6_ul1")}</li>
-                  <li>{t("instructions.signup.filling_out_desc6_ul2")}</li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>{t("instructions.signup.submitting")}</strong>
-            <ul>
-              <li>
-                <Trans
-                  i18nKey="instructions.signup.submitting_desc1"
-                  components={[<strong />]}
-                />
-              </li>
-              <li>{t("instructions.signup.submitting_desc2")}</li>
-              <li>
-                {t("instructions.signup.submitting_desc3")}
-                <ul>
-                  <li>{t("instructions.signup.submitting_desc3_ul1")}</li>
-                  <li>{t("instructions.signup.submitting_desc3_ul2")}</li>
-                  <li>{t("instructions.signup.submitting_desc3_ul3")}</li>
-                  <li>{t("instructions.signup.submitting_desc3_ul4")}</li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>{t("instructions.signup.additional_notes")}</strong>
-            <ul>
-              <li>
-                <Trans
-                  i18nKey="instructions.signup.additional_notes_desc1"
-                  components={[<strong />]}
-                />
-              </li>
-              <li>{t("instructions.signup.additional_notes_desc2")}</li>
-            </ul>
-          </li>
-        </ol>
-      </>
-    );
-  } else if (formName === "PartnerList" || formName === "PaymentRequestsList") {
-    let object = "Object";
-    let objects = "Objects";
-    let object_s = "Object's";
-    if (formName === "PartnerList") {
-      object = "Partner";
-      objects = "Partners";
-      object_s = "Partner's";
-    } else if (formName === "PaymentRequestsList") {
-      object = "Payment Request";
-      objects = "Payment Requests";
-      object_s = "Payment Request's";
-    }
-    return (
-      <>
+            )
+        )}
+        <li>
+          <strong>{t("instructions.loading_indicator")}</strong>
+          <ul>
+            <li>
+              <Trans
+                i18nKey="instructions.loading_indicator_desc1"
+                components={[<strong />]}
+              />
+            </li>
+            <li>{t("instructions.loading_indicator_desc2")}</li>
+          </ul>
+        </li>
+      </ol>
+      {instructions.instructions[nameInstruction]?.note && (
         <p>
           <Trans
-            i18nKey="instructions.object_list.introduction"
+            i18nKey={`instructions.${nameInstruction}.note`}
             values={{
               object,
               objects,
@@ -357,36 +119,7 @@ export const getInstructionByFormName = (formName = "", t, Trans) => {
             components={[<strong />]}
           />
         </p>
-        <ol>
-          {Object.entries(instructions.instructions.object_list).map(
-            ([key, section]) =>
-              key !== "introduction" && (
-                <li key={key}>
-                  <strong>{t(`instructions.object_list.${key}.title`)}</strong>
-                  <ul>
-                    {Object.entries(section).map(
-                      ([subKey, subDesc]) =>
-                        subKey !== "title" && (
-                          <li key={subKey}>
-                            <Trans
-                              i18nKey={`instructions.object_list.${key}.${subKey}`}
-                              values={{
-                                object,
-                                objects,
-                                object_s,
-                              }}
-                              components={[<strong />]}
-                            />
-                          </li>
-                        )
-                    )}
-                  </ul>
-                </li>
-              )
-          )}
-        </ol>
-      </>
-    );
-  }
-  return <></>;
+      )}
+    </>
+  );
 };
