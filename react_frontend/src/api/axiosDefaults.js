2eb77c4005bbaa469e2837543d45841001b6c419
BogdanovaIV <irinabogdanova261@gmail.com>
2025-03-03 21:07:25 +0500

    Add Cache-Control for Heroku

--- react_frontend/src/api/axiosDefaults.js
+++ react_frontend/src/api/axiosDefaults.js
@@ -7,6 +7,8 @@ if (process.env.NODE_ENV !== "development") {
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 axios.defaults.headers["Accept-Language"] = getLanguage() || "en";
+axios.defaults.headers["Cache-Control"] =
+  "no-store, no-cache, must-revalidate, proxy-revalidate";
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
